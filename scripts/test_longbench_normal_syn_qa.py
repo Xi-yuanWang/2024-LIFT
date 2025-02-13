6267e7a1cf9496c5c85188afa2ed384d43028a26
lumen <lumen2004.scholar@gmail.com>
2025-02-13 16:50:47 +0800

    Fix (prefix tuning, LongBench): Transfer parameters

--- scripts/test_longbench_normal_syn_qa.py
+++ scripts/test_longbench_normal_syn_qa.py
@@ -268,7 +268,7 @@ class LongBenchDataset(ICLContextDataset):
         return len(self.data) if self.enable_qa_tag else self.num_segments
     
 
-def LongBenchtrain(context: str, tokenizer: PreTrainedTokenizer, model_name_or_path: str, training_args: TrainingArguments, model_max_length: int=4096, block_size: int=256, len_segment: int=8, len_offset: int=3, use_lora: bool=False, lora_rank: Optional[int]=None, use_pissa: bool=False, load_in_4bit: bool=False, involve_qa_epochs: int=0, gather_batches: bool=True, num_syn_qa: int=0, use_gated_memory: bool=False, generator_name_or_path: Optional[str]=None, use_icl: bool=True, **kwargs):
+def LongBenchtrain(context: str, tokenizer: PreTrainedTokenizer, model_name_or_path: str, training_args: TrainingArguments, model_max_length: int=4096, block_size: int=256, len_segment: int=8, len_offset: int=3, use_lora: bool=False, lora_rank: Optional[int]=None, use_pissa: bool=False, load_in_4bit: bool=False, involve_qa_epochs: int=0, gather_batches: bool=True, num_syn_qa: int=0, use_gated_memory: bool=False, generator_name_or_path: Optional[str]=None, use_icl: bool=True, use_prefix_tuning: bool=False, num_virtual_tokens: Optional[int]=None, **kwargs):
     model = load_model(
         model_name_or_path=model_name_or_path,
         use_lora=use_lora,
@@ -276,7 +276,9 @@ def LongBenchtrain(context: str, tokenizer: PreTrainedTokenizer, model_name_or_p
         use_pissa=use_pissa,
         load_in_4bit=load_in_4bit,
         vocab_size=len(tokenizer),
-        use_gated_memory=use_gated_memory
+        use_gated_memory=use_gated_memory,
+        use_prefix_tuning=use_prefix_tuning,
+        num_virtual_tokens=num_virtual_tokens
     )
     if use_lora or use_gated_memory:
         dataset = LongBenchDataset(context, tokenizer, model_max_length, block_size, len_segment, len_offset, num_syn_qa, generator_name_or_path, use_icl)
