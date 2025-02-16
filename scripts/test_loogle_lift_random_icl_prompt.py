d6dc68ee9eb2ac4570617e57ec21dcb135ba355f
lumen <lumen2004.scholar@gmail.com>
2025-02-16 16:04:32 +0800

    Feat (LooGLE, prefix tuning): Enable prefix_tuning for LooGLE

--- scripts/test_loogle_lift_random_icl_prompt.py
+++ scripts/test_loogle_lift_random_icl_prompt.py
@@ -271,9 +271,19 @@ class LooGLEDataset(ICLContextDataset):
         return len(self.data) if self.enable_qa_tag else self.num_segments
     
     
-def LooGLEtrain(context: str, title: str, tokenizer: PreTrainedTokenizer, model_name_or_path: str, training_args: TrainingArguments, model_max_length: int=4096, block_size: int=256, len_segment: int=8, len_offset: int=3, use_lora: bool=False, lora_rank: Optional[int]=None, use_pissa: bool=False, load_in_4bit: bool=False, involve_qa_epochs: int=0, gather_batches: bool=True, num_syn_qa: int=0, title_option: int=1, generator_name_or_path: Optional[str]=None, use_gated_memory: bool=False, use_cot: bool=False, use_icl: bool=True, **kwargs):
-    model = load_model(model_name_or_path=model_name_or_path, use_lora=use_lora, lora_rank=lora_rank, use_pissa=use_pissa, load_in_4bit=load_in_4bit, vocab_size=len(tokenizer), use_gated_memory=use_gated_memory)
-    if use_lora or use_gated_memory:
+def LooGLEtrain(context: str, title: str, tokenizer: PreTrainedTokenizer, model_name_or_path: str, training_args: TrainingArguments, model_max_length: int=4096, block_size: int=256, len_segment: int=8, len_offset: int=3, use_lora: bool=False, lora_rank: Optional[int]=None, use_pissa: bool=False, load_in_4bit: bool=False, involve_qa_epochs: int=0, gather_batches: bool=True, num_syn_qa: int=0, title_option: int=1, generator_name_or_path: Optional[str]=None, use_gated_memory: bool=False, use_cot: bool=False, use_icl: bool=True, use_prefix_tuning: bool=False, num_virtual_tokens: Optional[int]=None, **kwargs):
+    model = load_model(
+        model_name_or_path=model_name_or_path,
+        use_lora=use_lora,
+        lora_rank=lora_rank,
+        use_pissa=use_pissa,
+        load_in_4bit=load_in_4bit,
+        vocab_size=len(tokenizer),
+        use_gated_memory=use_gated_memory,
+        use_prefix_tuning=use_prefix_tuning,
+        num_virtual_tokens=num_virtual_tokens
+    )
+    if use_lora or use_gated_memory or use_prefix_tuning:
         dataset = LooGLEDataset(context, title, tokenizer, model_max_length, block_size, len_segment, len_offset, num_syn_qa, title_option, generator_name_or_path, use_cot, use_icl=use_icl)
         model = train(model, dataset, tokenizer, training_args, involve_qa_epochs, gather_batches)[0]
     return model
