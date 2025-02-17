5dc0fa637d9646a3dcba80a9112376865a199c74
lumen <lumen2004.scholar@gmail.com>
2025-02-17 08:59:22 +0800

    Test (Vanilla Gate): rICL+QA0+C3M5

---
+++ shells/refine/VanGate-rICL-QA0-C3M5-passage_retrieval_en.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH -p IAI_SLURM_HGX
+#SBATCH -o logs/%j-VanGate-rICL-QA0-C8-passage_retrieval_en.out.log
+#SBATCH -e logs/%j-VanGate-rICL-QA0-C8-passage_retrieval_en.err.log
+#SBATCH --gres=gpu:4
+#SBATCH --qos=16gpu-hgx
+#SBATCH -J Q0C8p
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=24:00:00
+
+python scripts/mp_wrapper_longbench.py \
+    --script scripts/test_longbench_normal_syn_qa.py \
+    --num_process 4 \
+    --input_dir datasets/longbench_sampling \
+    --output_file outputs/VanGate/rICL-QA0-C8-passage_retrieval_en.jsonl \
+    --subprocess_args \
+    --subtask_name passage_retrieval_en \
+    --overwrite True \
+    --num_syn_qa 0 \
+    --generator_name_or_path models/Meta-Llama-3-8B-Instruct \
+    --use_icl True \
+    --model_name_or_path models/Gated-Memory-Llama-3-8B-Instruct \
+    --model_max_length 7800 \
+    --block_size 256 \
+    --len_segment 8 \
+    --len_offset 3 \
+    --use_gated_memory True \
+    --load_in_4bit True \
+    --gather_batches True \
+    --involve_qa_epochs 0 \
+    --num_train_epochs 8 \
+    --learning_rate 1e-3 \
+    --remove_unused_columns False \
+    --report_to none \
+    --output_dir models/temp \
+    --overwrite_output_dir True \
+    --per_device_train_batch_size 1 \
+    --weight_decay 1e-4 \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --log_level info \
+    --logging_strategy steps \
+    --logging_steps 1 \
+    --save_strategy no \
+    --bf16 True \
+    --tf32 False \
+    --gradient_checkpointing True \
+    --lr_scheduler_type constant
