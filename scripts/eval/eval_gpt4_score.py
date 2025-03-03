8dba702262f39d567b59527adb606dfc4c100501
lumen <lumen2004.scholar@gmail.com>
2025-03-03 12:49:10 +0800

    Fix: Update evaluation programs

--- scripts/eval/eval_gpt4_score.py
+++ scripts/eval/eval_gpt4_score.py
@@ -1,100 +1,151 @@
-import os
-import openai
-import tqdm
-import json
 import argparse
+import json
+import os
+from openai import OpenAI
+from typing import Dict
+from datetime import datetime
 
-with open('api_key.txt', 'r') as f:
-    api_key = f.read().strip()
-client = openai.Client(api_key=api_key)
 
-def get_gpt4_score(question: str, reference: str, pred: str):
-    sys_prompt = "Given one question, there is a groundtruth and a predict_answer. Please decide whether they are the same or not in semantic. Please only output 'True' or 'False' ."
+def parse_metadata(metadata_kwargs) -> Dict:
+    if metadata_kwargs is None:
+        return {}
+    if len(metadata_kwargs) % 2 == 1:
+        raise ValueError("Fail to match metadata_kwargs in pairs.")
+    return {key: value for key, value in zip(metadata_kwargs[::2], metadata_kwargs[1::2])}
 
-    prompt = [{"role": "system", "content": sys_prompt,},
-    {
-        "role": "user",
-        "content": "Question: "
-        + question
-        + "\n"
-        + "groundtruth = "
-        + reference
-        + "\n"
-        + "predict_answer = "
-        + pred,
-    }]
-    response = client.chat.completions.create(
-        model="gpt-4-0613",
-        messages=prompt,
-        max_tokens=10,
-        temperature=0,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
+
+def create_batch_file(client: OpenAI, input_path: str, metadata: Dict):
+    temp_file = '_temp_' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jsonl'
+    if os.path.exists(temp_file):
+        raise FileExistsError("Evaluation program tries to write the file `_temp.jsonl` as a temporary file but finds it exists.")
+    with open(input_path, 'r') as f:
+        data = [json.loads(l) for l in f]
+    with open(temp_file, 'w') as f:
+        PROMPT_TEMPLATE = "Question: {question}\ngroundtruth = {reference}\npredict_answer = {pred}"
+        for i, d in enumerate(data):
+            if 'sample' in d:
+                d = d['sample']
+            for j, qa_pair in enumerate(d['qa_pairs']):
+                messages = [
+                    {'role': 'system', 'content': "Given one question, there is a groundtruth and a predict_answer. Please decide whether they are the same or not in semantic. Please only output 'True' or 'False' ."},
+                    {'role': 'user', 'content': PROMPT_TEMPLATE.format(question=qa_pair['Q'], reference=qa_pair['A'], pred=qa_pair['pred'])}
+                ]
+                entry = {
+                    'custom_id': f'LooGLE-{i}-{j}',
+                    'method': 'POST',
+                    'url': '/v1/chat/completions',
+                    'body': {
+                        'model': 'gpt-4-0613',
+                        'messages': messages,
+                        'max_tokens': 10
+                    }
+                }
+                f.write(json.dumps(entry) + '\n')
+    print("Write to temp file. Submitting...")
+    
+    batch_input_file = client.files.create(
+        file=open(temp_file, 'rb'),
+        purpose='batch'
     )
-    response = response.choices[0].message.content
-    if not response:
-        print("Error: No response from GPT-4")
-    if 'True' in response or 'true' in response or 'TRUE' in response:
-        return True
+
+    batch_input_file_id = batch_input_file.id
+    batch_info = client.batches.create(
+        input_file_id=batch_input_file_id,
+        endpoint='/v1/chat/completions',
+        completion_window='24h',
+        metadata=metadata
+    )
+
+    print("Finish creating and submitting a Batch object.")
+    print("File object:")
+    print(batch_input_file)
+    print("Batch object:")
+    print(batch_info)
+    print(f"Please remember the ID attr. Batch ID = {batch_info.id}")
+    os.remove(temp_file)
+
+
+def retrieve_batch(client: OpenAI, batch_id, file_id, loogle_file, result_file):
+    os.makedirs(os.path.split(result_file)[0], exist_ok=True)
+    if batch_id is not None:
+        info = client.batches.retrieve(batch_id)
+        print("Retrieved info:")
+        print(info)
+        if info.status == 'completed':
+            print(f"Batch completed. Retrieving results from `{info.output_file_id}`...")
+            f_result = client.files.content(info.output_file_id)
+            print("Processing...")
+            data = f_result.read().decode()
+            data = [d for d in data.split('\n') if len(d) > 0]
+            data = [json.loads(l) for l in data]
+            data = {d['custom_id']: d for d in data}
+            with open(loogle_file, 'r') as f:
+                raw_data = [json.loads(l) for l in f]
+            for i, d in enumerate(raw_data):
+                if 'sample' in d:
+                    for j, q in enumerate(d['sample']['qa_pairs']):
+                        response = data[f'LooGLE-{i}-{j}']['response']['body']['choices'][0]['message']['content']
+                        q['score'] = 'true' in response.lower()
+                    d['meta_data']['score'] = sum([q['score'] for q in d['sample']['qa_pairs']]) / len(d['sample']['qa_pairs'])
+                else:
+                    for j, q in enumerate(d['qa_pairs']):
+                        response = data[f'LooGLE-{i}-{j}']['response']['body']['choices'][0]['message']['content']
+                        q['score'] = 'true' in response.lower()
+            with open(result_file, 'w') as f:
+                for d in raw_data:
+                    f.write(json.dumps(d) + '\n')
+            print(f"Write to {result_file}. Deleting OpenAI input and output files...")
+            client.files.delete(info.input_file_id)
+            client.files.delete(info.output_file_id)
+            print("Done.")
+        elif info.error_file_id is not None:
+            f_error = client.files.content(info.error_file_id)
+            print(f_error.read().decode())
+    elif file_id is not None:
+        info = client.files.retrieve(file_id)
+        print("Retrieved info:")
+        print(info)
+        f_result = client.files.content(file_id)
+        data = f_result.read().decode()
+        with open(result_file, 'w') as f:
+            f.write(data)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--action', required=True, choices=['submit', 'list', 'retrieve'])
+    parser.add_argument('--api_key', default=os.environ["GPT4_API_KEY"], help="GPT4 API KEY. Defaults to the envar `GPT4_API_KEY`.")
+    parser.add_argument('--loogle_file', help="LooGLE output file.")
+    parser.add_argument('--batch_id', help="The batch ID of a previous request.")
+    parser.add_argument('--file_id', help="The output file ID.")
+    parser.add_argument('--result_file', help="The result GPT_SCORE file.")
+    parser.add_argument('--metadata_kwargs', nargs='*', help="The following are metadata kwargs.")
+    args = parser.parse_args()
+    metadata = parse_metadata(args.metadata_kwargs)
+
+    client = OpenAI(api_key=args.api_key)
+
+    if args.action == 'submit':
+        if args.loogle_file is None:
+            raise ValueError("Please provide `--loogle_file`!")
+        create_batch_file(client, args.loogle_file, metadata)
+
+    elif args.action == 'list':
+        file_list = list(client.files.list())
+        if len(file_list) == 0:
+            print("Empty!")
+        else:
+            for i, entry in enumerate(file_list):
+                print(f"[{i}]")
+                print(entry)
+    
     else:
-        return False
+        if (args.batch_id is None and args.file_id is None) or args.result_file is None or args.loogle_file is None:
+            raise ValueError("Please provide `--batch_id` or `--file_id`, `--loogle_file`, and `--result_file`!")
+        if args.batch_id is not None and args.file_id is not None:
+            raise ValueError("Only one of `--batch_id` and `--file_id` should be specified.")
+        retrieve_batch(client, args.batch_id, args.file_id, args.loogle_file, args.result_file)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, required=True, help='Path to the input JSON/JSONL file.')
-    parser.add_argument('--output', type=str, required=True, help='Path to the output JSONL file.')
-    parser.add_argument('--overwrite', action='store_true', help='Overwrite the output JSON file.')
-    parser.add_argument('--num_test', type=int, default=None)
-    args = parser.parse_args()
-    
-    file_name, file_ext = os.path.splitext(args.input)
-    if file_ext == '.json':
-        with open(args.input, 'r') as f:
-            samples = json.load(f)
-    elif file_ext == '.jsonl':
-        samples = []
-        with open(args.input, 'r') as f:
-            for line in f:
-                samples.append(json.loads(line))
-    if args.num_test:
-        samples = samples[:args.num_test]
-    
-    # overwrite the output file
-    if args.overwrite and os.path.exists(args.output):
-        os.remove(args.output)
-
-    # resume the output file
-    gpt4_scores = []
-    num_resumed = 0
-    if os.path.exists(args.output):
-        with open(args.output, 'r') as f:
-            for sample in f.readlines():
-                num_resumed += 1
-                sample = json.loads(sample)
-                if 'qa_pairs' in sample:
-                    for qa_pair in sample['qa_pairs']:
-                        gpt4_scores.append(qa_pair['scores']['gpt4_score'])
-    
-    for sample in tqdm.tqdm(samples, total=len(samples), desc='Calculating GPT-4 score'):
-        if 'qa_pairs' in sample:
-            if num_resumed > 0:
-                num_resumed -= 1
-                continue
-            for qa_pair in sample['qa_pairs']:
-                question = qa_pair["Q"]
-                reference = qa_pair["A"]
-                pred = qa_pair["pred"]
-                gpt4_score = get_gpt4_score(question, reference, pred)
-                if 'scores' not in qa_pair.keys():
-                    qa_pair['scores'] = {}
-                    qa_pair['scores']['gpt4_score'] = gpt4_score
-                else:
-                    qa_pair['scores']['gpt4_score'] = gpt4_score
-                gpt4_scores.append(gpt4_score)
-            with open(args.output, 'a') as f:
-                f.write(json.dumps(sample) + '\n')
-    if len(gpt4_scores) > 0 and num_resumed == 0:
-        gpt4_score = sum(gpt4_scores) / len(gpt4_scores)
-        with open(args.output, 'a') as f:
-            f.write(json.dumps({'gpt4_score': gpt4_score}) + '\n')
+    main()
