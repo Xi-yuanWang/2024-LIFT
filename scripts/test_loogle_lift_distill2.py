1bc5aa403de7cf5e60b50dcace1f5ea272efce60
muhan01 <muhan01@headnode01.cm.cluster>
2025-03-18 19:29:04 +0800

    fix_cache_position

--- scripts/test_loogle_lift_distill2.py
+++ scripts/test_loogle_lift_distill2.py
@@ -344,7 +344,8 @@ class LooGLEDataset(ICLContextDataset):
     
 def LooGLEtrain(context: str, title: str, tokenizer: PreTrainedTokenizer, model_name_or_path: str, training_args: TrainingArguments, model_max_length: int=4096, block_size: int=256, len_segment: int=8, len_offset: int=3, use_lora: bool=False, lora_rank: Optional[int]=None, use_pissa: bool=False, load_in_4bit: bool=False, involve_qa_epochs: int=0, gather_batches: bool=True, num_syn_qa: int=0, title_option: int=1, generator_name_or_path: Optional[str]=None, use_gated_memory: bool=False, use_cot: bool=False, use_icl: bool=True, kv_epochs: int=0, distilldatapath: str=None, **kwargs):
     model = load_model(model_name_or_path=model_name_or_path, use_lora=use_lora, lora_rank=lora_rank, use_pissa=use_pissa, load_in_4bit=load_in_4bit, vocab_size=len(tokenizer), use_gated_memory=use_gated_memory)
-    
+    dataset = DistillDataset(title, context, tokenizer, model_max_length, block_size, len_segment, len_offset)
+    len_context = dataset[0]["len_context"] 
     if kv_epochs == 0:
         pass
     else:
@@ -362,8 +363,6 @@ def LooGLEtrain(context: str, title: str, tokenizer: PreTrainedTokenizer, model_
             if param.requires_grad:
                 print(name, param.shape, param.numel())
         model.save_pretrained(osp.join(training_args.output_dir, "before_distill"))
-        dataset = DistillDataset(title, context, tokenizer, model_max_length, block_size, len_segment, len_offset)
-        len_context = dataset[0]["len_context"]
         model = distilltrain2(model, dataset, tokenizer, training_args, kv_epochs, gather_batches, distilldatapath)[0]
         model.save_pretrained(osp.join(training_args.output_dir, "after_distill"))
     #dataset = LooGLEDataset(context, title, tokenizer, model_max_length, block_size, len_segment, len_offset, num_syn_qa, title_option, generator_name_or_path, use_cot, use_icl=use_icl)
